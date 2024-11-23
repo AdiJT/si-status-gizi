71ac4098fcdd72b01a2052631658762be85e4ed3
AdiJT <aditaklal@gmail.com>
2024-11-23 18:15:45 +0800

    Sedikit

---
+++ StatusGizi.Infrastructure/Database/ModelBuilderExtension.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StatusGizi.Domain.Entities;
+using StatusGizi.Domain.Enums;
+
+namespace StatusGizi.Infrastructure.Database;
+
+public static class ModelBuilderExtension
+{
+    public static ModelBuilder SeedingData(this ModelBuilder modelBuilder)
+    {
+        var daftarStandarBeratBadanBawah24 = new[]
+        {
+            new StandarBeratMenurutBBTB
+            {
+                Id = 1,
+                KategoriUmur = KategoriUmur.BulanBawah24,
+                JenisKelamin = JenisKelamin.LakiLaki,
+                TinggiBadan = 45,
+                MedianBeratBadan = 2.4,
+                SDMinus1 = 2.2,
+                SDMinus2 = 2,
+                SDMinus3 = 1.9,
+                SDPlus1 = 2.7,
+                SDPlus2 = 3,
+                SDPlus3 = 3.3,
+            }
+        };
+
+        var daftarStandarBeratBadan2460Bulan = new[]
+        {
+        };
+
+        modelBuilder.Entity<StandarBeratMenurutBBTB>().HasData(daftarStandarBeratBadanBawah24);
+        modelBuilder.Entity<StandarBeratMenurutBBTB>().HasData(daftarStandarBeratBadan2460Bulan);
+
+        return modelBuilder;
+    }
+}
